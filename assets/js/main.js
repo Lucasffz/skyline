20a66aebf8ce41decedcb01c2de01e3b09e9a74e
isaaccentenario <isaac.centenario@gmail.com>
2017-07-23 02:57:59 +0000

    index & graphs

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,4 +28,41 @@ $(function(){
 			localStorage.setItem('loggedIn', true);
 		}
 	})
+
+	if( $("#transactions").length > 0 ) {
+		var ctx = $("#transactions").get(0).getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'line',
+			responsive: true,
+			data: {
+				labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"],
+				datasets: [{
+					label: 'Movimentações em R$ nos últimos meses',
+					data: [12000, 19000, 3000, 5000, 14000, 3000],
+					// backgroundColor: [
+					// 	'rgba(255, 99, 132, 0.2)',
+					// 	'rgba(54, 162, 235, 0.2)',
+					// 	'rgba(255, 206, 86, 0.2)',
+					// 	'rgba(75, 192, 192, 0.2)',
+					// 	'rgba(153, 102, 255, 0.2)',
+					// 	'rgba(255, 159, 64, 0.2)'
+					// ],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+	}
+
+	$(".operations .nav a").click(function(){
+		$(this).tab('show');
+	})
 })
